dada7b6c27067aefec0e6df7b0e2db3f3c2c622e
Murdius <garylau.work@gmail.com>
2016-10-08 18:03:00 -0400

    Fix: reverting back to old one

--- app/__init__.py
+++ app/__init__.py
@@ -5,12 +5,8 @@ app = Flask(__name__, instance_relative_config=True)
 app.secret_key ='\xaf\xa6Q~\xd3\x88\xaa?k\xb5\xdd\xd0\x0c\x05\x93\x04\x16~\t\x8b\x90\xa9\x1dD'
 
 #app.config.from_object('config')
-app.config.from_pyfile('config.py')
-#app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+#app.config.from_pyfile('config.py')
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
 db = SQLAlchemy(app)
 
 from app import views
-
-
-
