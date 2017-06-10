241312f1fd8425a2fc08d821d730eef26788acc6
Murdius <garylau.work@gmail.com>
2017-06-10 18:21:28 -0400

    Updating disclaimer message.

--- app/static/components/disclaimer.jsx
+++ app/static/components/disclaimer.jsx
@@ -3,10 +3,8 @@ import React from 'react';
 class Disclaimer extends React.Component {
     render() {
         return (
-            <div className="disclaimer">
-                <p>
-                    Disclaimer: Each snapshot is inaccurate up to 5 mins.Do nothing for 5 mins before you take the first snapshot.Do nothing for 5 mins after your session and then take snapshot.Try clearing cookies if something breaks.
-                </p>
+            <div className="alert alert-warning" role="alert">
+                Disclaimer: Each snapshot is inaccurate up to 5 mins.Do nothing for 5 mins before you take the first snapshot.Do nothing for 5 mins after your session and then take snapshot.Try clearing cookies if something breaks.
             </div>
         );
     }
